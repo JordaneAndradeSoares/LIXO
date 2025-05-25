2f582b881507b8a888dbf7d28719b81b2b0f8383
JordaneAndradeSoares <113549535+JordaneAndradeSoares@users.noreply.github.com>
2025-05-24 21:17:01 -0300

    Add files via upload

--- amogus_voador/amogus_voador.py
+++ amogus_voador/amogus_voador.py
@@ -4,17 +4,17 @@ import sys
 
 pygame.init()
 
-## cores
+# cores
 PRETO = (0, 0, 0)
 VERMELHO = (255, 0, 0) 
 BRANCO = (255, 255, 255)
 
-## tela e fonte 
+# tela e fonte 
 LARGURA, ALTURA = 800, 400
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 fonte = pygame.font.SysFont("Arial", 40, True, True)
 
-## pulo e booleanos
+# pulo e booleanos
 contador_salto = 10
 salto_jogador = -10
 pulando = False
@@ -22,7 +22,7 @@ jogando = False
 resultado = True
 contador_derrota = False
 
-## outras variaveis 
+# outras variaveis 
 clock = pygame.time.Clock()
 
 velocidade_obstaculos = 10
@@ -36,33 +36,19 @@ jogador_y = ALTURA - tamanho
 
 obstaculo_x = LARGURA + tamanho
 obstaculo_y = ALTURA - tamanho
+obstaculo_y_reserva = obstaculo_y
 
 obstaculo_voador_x = obstaculo_x
 obstaculo_voador_y = ALTURA - (tamanho * 2.1)
 
-## classe zero (disco voador)
-class Rex_0(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.disco_voador = pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\4.png")
-
-        self.frame_count = 0
-        self.frame_rate = 5
-        self.atual = 0
-        self.image_voadora = self.disco_voador
-        self.image_voadora = pygame.transform.scale(self.image_voadora, (tamanho * 2.6, tamanho))
-
-        self.rect = self.image_voadora.get_rect()
-        self.rect.topleft = 100, 100
-
-## primeira classe 
+# classe zero (jogador)
 class Rex_1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = [
-            pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\1.png"),
-            pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\2.png"),
-            pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\3.png")
+            pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\1.png"),
+            pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\2.png"),
+            pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\3.png")
         ]
 
         self.frame_count = 0
@@ -85,15 +71,29 @@ class Rex_1(pygame.sprite.Sprite):
             self.image = self.sprites[self.atual]
             self.image = pygame.transform.scale(self.image, (tamanho, tamanho))
 
-## segunda classe
+# primeira classe (inimigo voador)
+class Rex_0(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.disco_voador = pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\4.png")
+
+        self.frame_count = 0
+        self.frame_rate = 5
+        self.atual = 0
+        self.image_voadora = self.disco_voador
+        self.image_voadora = pygame.transform.scale(self.image_voadora, (tamanho * 2.6, tamanho))
+
+        self.rect = self.image_voadora.get_rect()
+        self.rect.topleft = 100, 100
+
+# segunda classe (inimigo que anda)
 class Rex_2(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = [
-
-            pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\1.png"),
-            pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\2.png"),
-            pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\3.png")
+            pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\1.png"),
+            pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\2.png"),
+            pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\3.png")
         ]
 
         self.sprites_invertidos = [pygame.transform.flip(sprite, True, False) for sprite in self.sprites]
@@ -118,16 +118,16 @@ class Rex_2(pygame.sprite.Sprite):
             self.image = self.sprites_invertidos[self.atual]
             self.image = pygame.transform.scale(self.image, (tamanho, tamanho))
 
-# terceira classe (pulando)
+# terceira classe (pulo)
 class Pulando(pygame.sprite.Sprite):
     def som_do_pulo():
-        som_salto = pygame.mixer.Sound("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\smw_jump.wav")
+        som_salto = pygame.mixer.Sound("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\smw_jump.wav")
         som_salto.play()
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
-        self.sprites.append(pygame.image.load("C:\\Users\\Jordane AS\\Pictures\\codigos malucos\\amogus_voador\\1.png"))
+        self.sprites.append(pygame.image.load("C:\\Users\\Jordane AS\\OneDrive\\Imagens\\codigos malucos\\amogus_voador\\1.png"))
         self.atual = 0
         self.image = self.sprites[self.atual]
         self.image = pygame.transform.scale(self.image, (tamanho, tamanho))
@@ -144,13 +144,13 @@ class Pulando(pygame.sprite.Sprite):
         self.image = self.sprites[int(self.atual)]
         self.image = pygame.transform.scale(self.image, (tamanho, tamanho))
 
-voando_inimigo = pygame.sprite.Group()
-rex_inimigo_voador = Rex_0()
-voando_inimigo.add(rex_inimigo_voador)
-
 andando = pygame.sprite.Group()
 rex = Rex_1()
 andando.add(rex)
+
+voando_inimigo = pygame.sprite.Group()
+rex_inimigo_voador = Rex_0()
+voando_inimigo.add(rex_inimigo_voador)
 
 andando_inimigo = pygame.sprite.Group()
 rex_inimigo = Rex_2()
@@ -160,13 +160,12 @@ pulo = pygame.sprite.Group()
 pulinho = Pulando()
 pulo.add(pulinho)
 
-## loop da gameplay 
-while True:
+while True: # loop de gameplay 
     tela.fill(BRANCO)
     clock.tick(fps)
 
     # Nome da janela na qual o jogo roda 
-    pygame.display.set_caption("Amogus Voador - Dinossauro do Chrome") 
+    pygame.display.set_caption("Amogus Dinossauro do Chrome") 
 
     # comandos
     for evento in pygame.event.get():
@@ -175,29 +174,32 @@ while True:
             sys.exit()
        
         elif evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_k and not pulando and jogando:
-                Pulando.som_do_pulo()
-                contador_salto = 10 
-                pulando = True 
+            if evento.key == pygame.K_SPACE:
+                if jogando and not pulando: # comando pulo
+                    Pulando.som_do_pulo()
+                    contador_salto = 10 
+                    pulando = True 
                 
-            elif evento.key == pygame.K_k and not jogando:
-                jogando = True
+                if not jogando: # controle da derrota 
+                    jogando = True
 
-                if contador_derrota:
-                    pontos = 0 # zernado os pontos quando recomeça o jogo 
-                    numero_do_jogo += 1 # contando quantos jogos já tiveram
-                       
+                    if contador_derrota:
+                        pontos = 0 # zernado os pontos quando recomeça o jogo 
+                        numero_do_jogo += 1 # contando quantos jogos já tiveram
+ 
+    # gameplay             
     if jogando:
 
-        # Defina o retângulo do jogador
+        # Define o retângulo do jogador
         jogador_rect = pygame.Rect(jogador_x, jogador_y, tamanho, tamanho)
 
-        # Defina os retângulos dos obstáculos
+        # Define os retângulos dos obstáculos
         obstaculo_rect = pygame.Rect(obstaculo_x, obstaculo_y, tamanho, tamanho)
         obstaculo_voador_rect = pygame.Rect(obstaculo_voador_x, obstaculo_voador_y, tamanho * 2.6, tamanho)
 
-        # Verifique as colisões
+        # Verifica as colisões
         if jogador_rect.colliderect(obstaculo_rect) or jogador_rect.colliderect(obstaculo_voador_rect):
+            
             contador_derrota = True
             jogando = False
 
@@ -237,6 +239,9 @@ while True:
             pontos += 1
         
         else:
+            # colocando jogador na tela
+            tela.blit(rex.image, (jogador_x, jogador_y))
+
             if resultado: 
                 # colocando obstaculo corredor na tela
                 obstaculo_x -= velocidade_obstaculos
@@ -245,9 +250,6 @@ while True:
                 # colocando obstaculo voador na tela
                 obstaculo_voador_x -= velocidade_obstaculos
                 tela.blit(rex_inimigo_voador.image_voadora, (obstaculo_voador_x, obstaculo_voador_y))
-
-            # colocando jogador na tela
-            tela.blit(rex.image, (jogador_x, jogador_y))
 
         mensagem = f"Pontos: {pontos}"
         texto = fonte.render(mensagem, True, PRETO)
@@ -259,8 +261,13 @@ while True:
             texto_2 = fonte.render(mensagem_2, True, VERMELHO)
             tela.blit(texto_2, (LARGURA // 10 , ALTURA // 10))
 
-        mensagem = "Pressione 'k' para jogar e pular"
+        mensagem = "Pressione espaço para jogar e pular"
         texto = fonte.render(mensagem, True, PRETO)
-        tela.blit(texto, (LARGURA // 10 , ALTURA // 4))
+        tela.blit(texto, (LARGURA // 14, ALTURA // 4))
+
+        # Resetando jogador
+        jogador_y = ALTURA - tamanho
+        contador_salto = 10
+        pulando = False
 
     pygame.display.update()
