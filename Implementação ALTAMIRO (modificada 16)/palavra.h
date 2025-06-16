560d361500cdfa35a445b40df7472ac992e0484a
JordaneAndradeSoares <113549535+JordaneAndradeSoares@users.noreply.github.com>
2025-06-16 08:19:38 -0300

    Delete Implementação ALTAMIRO (modificada 16) directory

--- Implementação ALTAMIRO (modificada 16)/palavra.h
+++
@@ -1,9 +0,0 @@
-// O grupo deverá estar identificado no cabeçalho de TODOS os arquivos do código-fonte:
-// Jordane Andrade Soares
-
-#ifndef PALAVRA
-#define PALAVRA
-
-typedef char Palavra[200];
-
-#endif
